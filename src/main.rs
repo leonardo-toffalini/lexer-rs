132017e20928e879f54370dced79c7e736aa995c
leonardo-toffalini <leonardotoffalini@gmail.com>
2025-07-15 21:32:25 +0200

    add file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::{fs::File, io::Read};
+
+fn read_file_contents(path: &str) -> Result<String, io::Error> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    return Ok(contents);
+}
+
+fn main() -> Result<(), io::Error> {
+    let contents = read_file_contents("examples/first.mk")?;
+    println!("File contents: {}", contents);
+    Ok(())
 }
